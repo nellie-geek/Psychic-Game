d3e85175f24aee5f48fcedd25864a632f460adc7
nellie-geek <janellegieck@msn.com>
2019-03-03 19:20:49 -0700

    More updates and funciton.

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,50 +1,47 @@
     // Creates an array that lists out all of the letters.
     var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     
     // Creating variables to hold the number of wins and losses. 
     var wins = 0;
     var losses = 0;
-    var guessesleft = 10; //Guesses does not display or start at designated value
+    var guesses = 12; 
     var guessedSoFar = [];
+    //At page load values do not display/only a single letter is being displayed onkeyup event//
     
     // Create variables to display things.
     var winsText = document.getElementById("wins-text");
     var lossesText = document.getElementById("losses-text");
-    var guessesLeftText = document.getElementById("guessesleft-text");
-    var guessedSoFarText = document.getElementById("guessedsofar-text");
-    var userText = document.getElementById("usertext");
-
-
-    
+    var guessesText = document.getElementById("guesses-text");
+    var guessedSoFarText = document.getElementById("key");
 
     // Randomly chooses a choice from the options array. This is the Computer's guess.
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
     // Key function.
-    document.onkeyup = function (event) {   
+    document.onkeyup = function (event) {  
 
         // Determines which key was pressed.
         var userGuess = event.key;
         guessedSoFar.push(userGuess);
         document.getElementById("key").textContent = userGuess;
-        
-
+            
         // Game logic and scoring.
         if (userGuess === computerGuess) {
             wins++;
-            guessesleft = 10;
+            guesses = 12;
 
         } else {
-            guessesleft--;
-        
-        } if (guessesleft === 0) {
-            losses++;
-            guessesleft = 10;
-        }
-
+            guesses--;
             
+        } if (guesses === 0) {
+            losses++;
+            guesses = 12;
+        }
+       
         // Display the user and score.
-        userGuess.textContent = "guessedsofar" + userGuess;
+        guessedSoFarText.textContent = userGuess;
+        guessesText.textContent = "Guesses remaining: " + guesses;
         winsText.textContent = "Wins: " + wins;
         lossesText.textContent = "Losses: " + losses;
     };
